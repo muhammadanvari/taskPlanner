dfc6d5b7d4117f6a65c9f054d30c178c02825f48
Syed <muhammadifreelancer00@gmail.com>
2026-02-15 15:16:25 +0330

    categories table change name

--- database/migrations/2026_01_08_070028_create_categories_table.php
+++ database/migrations/2026_01_08_070028_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
-            $table->string('color')->default('blue');
+            $table->string('color')->default('indigo');
             $table->timestamps();
         });
     }
